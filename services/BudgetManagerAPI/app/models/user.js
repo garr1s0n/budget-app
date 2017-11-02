87c512cddfae0667da3b74aa23930f505cc94d5d
garr1s0n <gary.wilpizeski@gmail.com>
2017-11-02 17:32:35 -0400

    db setup

--- services/BudgetManagerAPI/app/models/user.js
+++ services/BudgetManagerAPI/app/models/user.js
@@ -14,20 +14,20 @@ const Schema = mongoose.Schema({
 
 // We won't use arrow functions here because of automatic lexical scope binding
 Schema.pre('save', function (next) {
-  const user = this;
+    const user = this;
 
-  if (this.isModified('password') || this.isNew) {
-    bcrypt.genSalt(10, (error, salt) => {
-      if (error) return next(error);
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, (error, salt) => {
+            if (error) return next(error);
 
-      bcrypt.hash(user.password, salt, (error, hash) => {
-        if (error) return next(error);
+            bcrypt.hash(user.password, salt, (error, hash) => {
+                if (error) return next(error);
 
-        user.password = hash;
-        next();
-      });
-    });
-  } else return next();
+                user.password = hash;
+                next();
+            });
+        });
+    } else return next();
 });
 
 Schema.methods.comparePassword = function (password, callback) {
