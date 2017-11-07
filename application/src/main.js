4ebb696aab2a95b373d58ffee8300ff5f11a3b00
garr1s0n <gary.wilpizeski@gmail.com>
2017-11-07 14:02:53 -0500

    setting up front end auth component

--- application/src/main.js
+++ application/src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueCookie from 'vue-cookie'
+import Vuetify from 'vuetify'
+
+Vue.use(VueCookie)
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
