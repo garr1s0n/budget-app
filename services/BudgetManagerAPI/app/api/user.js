fb0eb78766e35016458770672fc89d57726bb23f
garr1s0n <gary.wilpizeski@gmail.com>
2017-11-07 21:21:28 -0500

    Budget Components + Routes + API components

--- services/BudgetManagerAPI/app/api/user.js
+++ services/BudgetManagerAPI/app/api/user.js
@@ -3,18 +3,18 @@ const mongoose = require('mongoose');
 const api = {};
 
 api.setup = (User) => (req, res) => {
-    const admin = new User({
-        username: 'admin',
-        password: 'admin',
-        clients: []
-    });
-
-    admin.save(error => {
-        if (error) throw error;
-
-        console.log('Admin account was successfully set up');
-        res.json({ success: true });
-    });
+    if (!req.body.username || !req.body.password) res.json({ success: false, message: 'Please, pass an username and password.' });
+    else {
+      const user = new User({
+        username: req.body.username,
+        password: req.body.password
+      });
+  
+      user.save(error => {
+        if (error) return res.status(400).json({ success: false, message: 'Username already exists.' });
+        res.json({ success: true, message: 'Account created successfully' });
+      });
+    }
 }
 
 api.index = (User, BudgetToken) => (req, res) => {
